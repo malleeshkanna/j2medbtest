b652bfead160a9b956b3e554b1329a9ed05544dc
malleeshkanna <88141142+malleeshkanna@users.noreply.github.com>
2023-01-26 23:24:39 +0530

    Update j2medb.js

--- j2medb.js
+++ j2medb.js
@@ -1,8 +1,10 @@
-const express=require('express');
 const http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.write("Welcome Malleesh");
+    if(req.url=="/malleesh"){
+        res.write("Welcome Malleesh");
+    }
     res.end();
+
   }).listen(8080);
