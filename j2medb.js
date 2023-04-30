5a3560c42d19127fd9dce5694e1bedc1f980fde9
malleeshkanna <88141142+malleeshkanna@users.noreply.github.com>
2023-04-30 23:20:54 +0530

    Update j2medb.js

--- j2medb.js
+++ j2medb.js
@@ -3,8 +3,16 @@ const axios=require("axios");
 const express=require("express");
 const cors=require("cors");
 const app=express();
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
 app.use(express.json());
 app.use(cors());
+
 
 app.get('/gstdetails/:gstno',async function(req,res){
     var url="https://irisgst.com/gstin-filing-detail/?gstinno="+req.params.gstno;
@@ -25,6 +33,20 @@ app.get('/gstdetails/:gstno',async function(req,res){
     }
 })
 
-app.listen("4000",()=>{
-    console.log("Port is connected");
+app.get("/openaiData/:ques",async function(req,res){
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.params.ques,
+        max_tokens:3000
+      });
+      res.json({data:completion.data.choices[0].text})
 })
+
+
+app.listen(5000,()=>{
+    console.log("Port is connected")
+})
+
+
+
+
